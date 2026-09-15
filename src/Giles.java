a89ffcd6ee32da2b52d1931756f86368f6fde8f8
Sam Johnson <smarty.sam.johnson@gmail.com>
2026-09-15 10:41:45 -0600

    Class Cleanup
    Removed unused enum Intents and added comments for clarity.

--- src/Giles.java
+++ src/Giles.java
@@ -1,23 +1,13 @@
-
 public class Giles{
    public static void main(String args[]){      
       Console UI = new Console("Giles, Text Input");
       //add check to see time of day. E.g. morning, afternoon, evening
-      UI.prompt("Good morning, sir. How may I be of assistance?");
       String input;
-      OptimusPrime panzer = new OptimusPrime(".data/Optimus/");
+      UI.prompt("Good morning, sir. How may I be of assistance?");//opening prompt
       while(true){
          input = UI.getInput();
-         UI.prompt(String.valueOf((int)'a'));
+         ICBM.process(input);
+         UI.prompt("Okay, so, I can't talk back yet. But see console for my interpretation of your intent!");
       }
    }
-}
-enum Intents{
-   CHAT,//default intent, doesn't match any other intent
-   MATH,//perform basic math operations
-   WEATHER,//check the weather
-   READ,//read text from a website
-   OPEN_FILE;//find and open a file on the current hardware
-   
-   private Intents(){}
 }
